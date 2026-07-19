5458b991b9bf09d2caa951a3341840aaec802f4a
masato <masato1216ss@gmail.com>
2026-07-19 18:27:57 +0900

    タスク編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,26 @@
 class TasksController < ApplicationController
 
   def index
-    @task= Task.order(created_at: :desc)
+    @tasks= Task.order(created_at: :desc)
   end
 
   def show
     @task= Task.find(params[:id])
   end
+
+  def edit
+    @task= Task.find(params[:id])
+  end
+
+  def update
+    @task= Task.find(params[:id])
+    if @task.update(title:params[:task][:title], description:params[:task][:description])
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
   
   def new
     @task= Task.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get "tasks/new"
   root "home#index"
-  # get 'tasks/new', to: 'tasks#new', as: :new_task
-  # post 'tasks/new', to: 'tasks#create'
+  # get 'tasks/new', to:'tasks#new', as: :new_task
+  # post 'tasks/new', to:'tasks#create'
   # get 'tasks', to:'tasks#index', as: :tasks
   # get 'tasks/:id', to:'tasks#show', as: :task
+  # get 'tasks/:id/edit', to:'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
   resources :tasks
 end
